3ef019e01d4630e4cbb89f70a5782eae95bb756b
sandro <iashvilisandro7@gmail.com>
2024-05-09 23:09:58 +0400

    Add conection handler

--- sql/connection_handler.py
+++ sql/connection_handler.py
@@ -1,6 +1,28 @@
 import pymysql
 
-con = pymysql.connect(host='localhost',
-                      user='root', password='sandroo1231',
-                      database='students')
-cur = con.cursor()
+
+def sql_connection():
+    con = pymysql.connect(host='localhost',
+                          user='root',
+                          password='sandroo1231',
+                          database='students')
+
+    return con
+
+
+def sql_names():
+    con = pymysql.connect(host='localhost',
+                          user='root',
+                          password='sandroo1231',
+                          database='students')
+
+    cursor = con.cursor()
+
+
+    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'students'")
+    results = cursor.fetchall()
+    column_names = [result[0] for result in results]
+
+    cursor.close()
+    con.close()
+    return column_names
